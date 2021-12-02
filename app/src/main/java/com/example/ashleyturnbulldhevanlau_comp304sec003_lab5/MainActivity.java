634a05d182e6425d0ddef8c1c20f71a9f56735ef
Ashley T <28864931+Sirusthevirus@users.noreply.github.com>
2021-12-02 00:01:46 -0500

    Added clickable carousel method using intents

--- app/src/main/java/com/example/ashleyturnbulldhevanlau_comp304sec003_lab5/MainActivity.java
+++ app/src/main/java/com/example/ashleyturnbulldhevanlau_comp304sec003_lab5/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.ashleyturnbulldhevanlau_comp304sec003_lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
@@ -19,14 +27,17 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private String[] imageTitle = new String[] {
-            "Alcohol, Asian, Burger, Ice-Cream, Mexican, Pizza, Street-Food"
+            "Alcohol", "Asian", "Burger", "Ice-Cream", "Mexican", "Pizza", "Street-Food"
     };
+
+    private static final int PERMISSION_REQUEST_LOCATION = 0;
+    private View mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mLayout = findViewById(R.id.layout);
 
         CarouselView carouselView = findViewById(R.id.carouselview);
         carouselView.setPageCount(images.length);
@@ -39,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
         carouselView.setImageClickListener(new ImageClickListener() {
             @Override
             public void onClick(int position) {
-
-                Toast.makeText(MainActivity.this, imageTitle[position], Toast.LENGTH_SHORT).show();
+                String foodItem = imageTitle[position];
+                searchNearby(foodItem);
             }
         });
     }
@@ -50,8 +61,19 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void searchNearby(View view) {
-        Intent intent = new Intent(this, MapsActivity.class);
+    public void searchNearby(String foodItem) {
+        Intent intent = new Intent(this, RestaurantListActivity.class);
+//        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+//        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//        Double longitude = location.getLongitude();
+//        Double latitude = location.getLatitude();
+        Double longitude = 43.6532;
+        Double latitude = 79.3832;
+        String longit = Double.toString(longitude);
+        String lat = Double.toString(latitude);
+        intent.putExtra("long", longit);
+        intent.putExtra("lat", lat);
+        intent.putExtra("food", foodItem);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/ashleyturnbulldhevanlau_comp304sec003_lab5/RestaurantListActivity.java
+++ app/src/main/java/com/example/ashleyturnbulldhevanlau_comp304sec003_lab5/RestaurantListActivity.java
@@ -2,6 +2,7 @@ package com.example.ashleyturnbulldhevanlau_comp304sec003_lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class RestaurantListActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class RestaurantListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_list);
+        Intent intent = getIntent();
+        String longitude = intent.getStringExtra("long");
+        String latitude = intent.getStringExtra("lat");
+        String foodType = intent.getStringExtra("food");
     }
 }
